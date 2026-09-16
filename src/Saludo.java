febea3e5bb5fd000a499958ec320c196a8c121cb
nicolashdz28 <eduardo.hdez287@gmail.com>
2026-09-16 21:23:40 +0000

    Ajusta el mensaje de saludo

--- src/Saludo.java
+++ src/Saludo.java
@@ -1,5 +1,5 @@
 public class Saludo {
     public static void main(String[] args) {
-        System.out.println("Hola soy Nico");
+        System.out.println("Hola soy Nico, y esta es la modificación del paso 7");
     }
 }
